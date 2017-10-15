4698b29e995f956ffae6315059a17ce0e0278201
Unknown <fabio.simplis@gmail.com>
2017-10-15 21:48:02 -0200

    Ordenando as aulas de um curso

--- src/br/com/alura/TestaCurso.java
+++ src/br/com/alura/TestaCurso.java
@@ -1,5 +1,8 @@
 package br.com.alura;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
 
 public class TestaCurso {
@@ -8,7 +11,7 @@ public class TestaCurso {
 		
 		Curso prog = new Curso("Prog1", "José");
 		
-		List<Aula> aulas = prog.getAulas();
+		List<Aula> aulasImutaveis = prog.getAulas();
 		
 		prog.adiciona(new Aula("Tipos de Dados", 30));
 		prog.adiciona(new Aula("Declaracao de variaveis", 15));
@@ -17,6 +20,18 @@ public class TestaCurso {
 		
 		//adiciona aulas
 		//aulas.add(new Aula("Trabalhando com array", 21));
+		System.out.println(aulasImutaveis);
+		
+		//Parte 2
+		System.out.println("\n < PARTE 2 > \n");
+		
+		List<Aula> aulas = new ArrayList<Aula>(aulasImutaveis);
+		
+		Collections.sort(aulas);
 		System.out.println(aulas);
+		
+		
+		
+		
 	}
 }

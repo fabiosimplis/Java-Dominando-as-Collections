9cd0135ae4c34b5dd1d752a16c75f3a7e4f33a69
Unknown <fabio.simplis@gmail.com>
2017-10-16 17:31:03 -0200

    Elementos duplicados

--- src/br/com/alura/ImprimindoAlunos.java
+++ src/br/com/alura/ImprimindoAlunos.java
@@ -16,6 +16,24 @@ public class ImprimindoAlunos {
         for (String aluno : alunos) {
 			System.out.println(aluno);
 		}
+        
+        //1) adicione alguns alunos
+        alunos.add("Jao");
+        alunos.add("Fabio");
+        alunos.add("Junior");
+        alunos.add("Alex");
+
+        //2) imprima o tamanho da colecao
+        
+        System.out.println("\nTamanho do set: " + alunos.size());
+        
+        //3) tente adicionar um aluno que existe
+        System.out.println("\nTetando adicionar mais um aluno no set:");
+        boolean adicionou = alunos.add("Fabio");
+        System.out.println("Adicionou? " + adicionou);
+        
+        //4) imprima novamente o tamanho da colecao
+        System.out.println("\nTamanho do set: " + alunos.size());
 	}
 
 }

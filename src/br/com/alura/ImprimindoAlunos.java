d535a90a3df31fcb7aca8a666093b8ce54870681
Unknown <fabio.simplis@gmail.com>
2017-10-16 17:20:55 -0200

    Imprimindo conjunto

---
+++ src/br/com/alura/ImprimindoAlunos.java
@@ -0,0 +1,21 @@
+package br.com.alura;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class ImprimindoAlunos {
+
+	public static void main(String[] args) {
+		Set<String> alunos = new HashSet<>();
+        alunos.add("Paulo");
+        alunos.add("Alberto");
+        alunos.add("Pedro");    
+        alunos.add("Nico");
+        
+        System.out.println("Imprime alunos: ");
+        for (String aluno : alunos) {
+			System.out.println(aluno);
+		}
+	}
+
+}
